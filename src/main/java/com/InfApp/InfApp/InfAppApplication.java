2e8f0d744613eaa44671b0dd2b987e60b9271564
Konradek <konradbiznesu@gmail.com>
2020-10-23 11:42:20 +0200

    fixing places
    xd

--- src/main/java/com/InfApp/InfApp/InfAppApplication.java
+++ src/main/java/com/InfApp/InfApp/InfAppApplication.java
@@ -1,13 +1,41 @@
-package com.InfApp.InfApp;
+package pl.infapp.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.infapp.demo.entity.Corona;
+import pl.infapp.demo.entity.User;
+import pl.infapp.demo.repository.UserRepository;
 
 @SpringBootApplication
-public class InfAppApplication {
+public class InfappApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InfAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InfappApplication.class, args);
+    }
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //Dodawanie użytkownioka w przyszlosci w controllerze
+        User user = new User();
+        user.setName("Konrad");
+        user.setEmail("konrad@gmail.com");
+
+        Corona corona = new Corona();
+        corona.setLink1("1");
+        corona.setLink2("2");
+        corona.setLink3("3");
+        corona.setLink4("4");
+
+        user.setCorona(corona);
+        corona.setUser(user);
+
+        userRepository.save(user);
+
+    }
 }
