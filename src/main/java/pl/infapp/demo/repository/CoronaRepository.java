bca7c56420cdf4f43dd736b2b4bed95eb23745ce
Konradek <konradbiznesu@gmail.com>
2020-10-23 11:46:04 +0200

    ogarniecie zycia
    wywalenie duplikatow

--- src/main/java/pl/infapp/demo/repository/CoronaRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.infapp.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.infapp.demo.entity.Corona;
-
-@Repository
-public interface CoronaRepository extends JpaRepository<Corona, Long> {
-}
--- src/main/java/pl/infapp/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.infapp.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.infapp.demo.entity.User;
-
-@Repository//do dodawnia uzywnikow
-public interface UserRepository extends JpaRepository <User, Long> {
-}
